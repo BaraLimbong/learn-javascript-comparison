e45f5d64920baaec3c12ebdda4cc56d373640353
Bara Limbong <baralimbong@gmail.com>
2019-01-30 16:03:53 +0700

    learn-javascript-number

---
+++ README.md
@@ -0,0 +1,3 @@
+# JAVASCRIPTCOMPARISON
+
+## Javascriptcomparisonpractice
---
+++ index.js
@@ -0,0 +1,15 @@
+const food = 'burger'
+if (food === 'burger') {
+  console.log('when you feel starving')
+} else if ('noodle') {
+  console.log('not yet hungry')
+} else {
+  console.log('every food is for all human mankind')
+}
+console.log('----------------')
+
+true ? console.log('true') : console.log('false')
+
+2 === 6 / 2 ? console.log('true') : console.log('false')
+
+console.log('--------------')
