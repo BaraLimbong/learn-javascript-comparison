aa1c78d8ea330a88b77333b621c36feb2476e2db
Bara Limbong <baralimbong@gmail.com>
2019-01-30 18:07:08 +0700

    create initial javascript

--- index.js
+++ index.js
@@ -13,3 +13,26 @@ true ? console.log('true') : console.log('false')
 2 === 6 / 2 ? console.log('true') : console.log('false')
 
 console.log('--------------')
+
+const cars = 'ferarri' || 'avanza'
+console.log(cars)
+
+const age = 80
+const car = 'innova'
+if (age >= 50 && car === 'innova') {
+  console.log('welcome brother')
+} else {
+  console.log('please try again')
+}
+
+console.log('-------------')
+
+const school = 'impactbyte'
+const coach = 'bara'
+
+if (school === 'impactbyte' || coach === 'bara') {
+  console.log('welcome to BARATECH CORP')
+}
+{
+  console.log('im sorry, try to others corp,see you!')
+}
